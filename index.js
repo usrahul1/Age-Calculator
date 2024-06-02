c8b30375aaab82d97993c778dc71782fd49c14a6
Sai Rahul Urumu <123154452+usrahul1@users.noreply.github.com>
2024-06-02 14:16:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
+const dateInput = document.getElementById('input');
+const today = new Date().toISOString().split('T')[0];
+dateInput.setAttribute('max', today);
+
+
 function funMyAge(){
+
     let x = document.getElementById("input").value;
     let ip = new Date(x);
     let y = document.getElementById("output");
 
+    if (!Date.parse(x)) {
+        alert("Please enter a valid date.");
+        return;
+    }
+
     let z = new Date();
 
     let ay = z.getFullYear()-ip.getFullYear();
+    let am = z.getMonth()-ip.getMonth();
+    let ad = z.getDate()-ip.getDate();
 
-    if(ip.getMonth() > z.getMonth()){
-        
+
+    if(am<0 || (am===0 && ad < 0)){
+        ay--;
+        am+=12;
     }
 
-    let am = z.getMonth()-ip.getMonth();
-    let ad = z.getDay()-ip.getDay();
+    if (ad < 0){
+        am--;
+        let dd = new Date(ip.getFullYear(), ip.getMonth(), 0);
+        ad += dd.getDate();
+    }
 
-
-    y.innerHTML = "Your age is "+ ay +" " +am+  " "+ad+  " ";
+    y.innerHTML = "Your age is "+ ay +" years " +am+  " months "+ad+  " days.";
 
 }
